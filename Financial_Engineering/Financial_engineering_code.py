3ae6f46e3b9770476ff2bc6d026173f0fe30f58a
Harneet <harneetsinghlamba@gmail.com>
2025-11-28 16:13:14 -0500

    Commiting the change in the code

--- Financial_Engineering/Financial_engineering_code.py
+++ Financial_Engineering/Financial_engineering_code.py
@@ -8,3 +8,5 @@ data = data.dropna(subset=['Consumer complaint narrative'])
 data = data.sample(n=1000, random_state=42)
 data = data.loc[data['Product'].isin(['Credit reporting, credit repair services, or other personal consumer reports',
                                      'Debt collection'])]
+clean_data = data[['Product', 'Consumer complaint narrative']].copy()
+clean_data['addressed_narrative'] = clean_data['Consumer complaint narrative'].str.lower()
